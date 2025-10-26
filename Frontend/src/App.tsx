a91ddae73ec337e0d53471fbe4368c53c2874588
anthonyRanivoarison <antonitsiory@gmail.com>
2025-10-26 14:14:29 +0300

    update : app router, add static images

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./app/home"
 import WebsiteLayout from "./app/layouts/website-layout";
+import LoginPage from "./app/login";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<WebsiteLayout><Home /></WebsiteLayout>} />
+        <Route path="/login" element={<WebsiteLayout><LoginPage /></WebsiteLayout>} />
       </Routes>
     </Router>
   );
