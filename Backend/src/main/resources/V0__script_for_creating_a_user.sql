943d79e8830a84e8048fb9c66fe88e6591984316
NomenaFitahiana <nomenafitahiana.randrianirina@gmail.com>
2025-10-31 13:58:54 +0300

    add script for user db creation

---
+++ Backend/src/main/resources/V0__script_for_creating_a_user.sql
@@ -0,0 +1,21 @@
+-- 1. Créer le rôle
+CREATE ROLE flyway_user WITH LOGIN PASSWORD 'School_userPassword';
+
+-- 2. Créer la base et assigner la propriété
+CREATE DATABASE school_db OWNER flyway_user;
+
+-- 3. Connexion et droits
+\c school_db postgres  -- se connecter à la base
+
+-- Droits sur le schéma public
+GRANT USAGE, CREATE ON SCHEMA public TO flyway_user;
+
+-- Droits sur les tables et séquences existantes
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON ALL TABLES IN SCHEMA public TO flyway_user;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO flyway_user;
+
+-- Droits par défaut pour les futures tables et séquences
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO flyway_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO flyway_user;
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
