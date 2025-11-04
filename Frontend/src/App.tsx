11f7ed26e3d8f1044af89effab4747d87458884b
anthonyRanivoarison <antonitsiory@gmail.com>
2025-11-04 14:40:16 +0300

    Update app router, create home page and install sonner toast

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,15 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./app/home"
-import WebsiteLayout from "./app/layouts/website-layout";
-import LoginPage from "./app/login";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<WebsiteLayout><Home /></WebsiteLayout>} />
-        <Route path="/login" element={<WebsiteLayout><LoginPage /></WebsiteLayout>} />
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
