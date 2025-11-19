e7cf07eb5cb8b49b2f159c852db9a6f16a1f0927
anthonyRanivoarison <antonitsiory@gmail.com>
2025-11-19 13:34:31 +0300

    refactor(shared): update filter, delete button, search and relations components, update core pages and layout

--- Frontend/src/app/site-announcement.tsx
+++ Frontend/src/app/site-announcement.tsx
@@ -1,8 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import type { Course } from "@/types/course";
-import { courseService } from "@/services/courses";
 import {
   Empty,
   EmptyDescription,
@@ -11,25 +8,18 @@ import {
   EmptyTitle,
 } from "@/components/ui/empty"
 import { BookX } from "lucide-react";
+import { useAnnouncements } from "@/hooks/use-announcement";
 import { Spinner } from "@/components/ui/spinner";
 
-export default function CoursesPage() {
-  const [courses, setCourses] = useState<Course[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    courseService.getAll().then((courses) => {
-      setLoading(false);
-      setCourses(courses);
-    });
-  }, []);
+export default function SiteAnnouncementsPage() {
+  const { announcements, loading } = useAnnouncements();
 
   return (
     <div className="p-12 space-y-6">
       <div className="flex flex-start md:items-center md:gap-0 gap-3 justify-between flex-col md:flex-row">
         <div className="space-y-2">
-          <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 text-(--blue)">Cours disponibles</h2>
-          <p className="text-sm text-muted-foreground">Parcourez les cours proposés cette année</p>
+          <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 text-(--blue)">Liste des annonces</h2>
+          <p className="text-sm text-muted-foreground">Informations importantes et communiqués au sein d'<strong>Omega school</strong></p>
         </div>
       </div>
 
@@ -37,16 +27,16 @@ export default function CoursesPage() {
         {loading ? (
           <div className="col-span-full flex flex-col gap-4 justify-center text-muted-foreground items-center min-h-[40vh]">
             <Spinner className="size-8" />
-            Chargement des cours. Veillez patientez un moment...
+            Chargement des annonces. Veillez patientez un moment...
           </div>
-        ) : courses.length > 0 ? courses.map((c) => (
-          <Card key={c.id} className="hover:shadow-lg transition max-w-lg">
+        ) : announcements.length > 0 ? announcements.map((announcement) => (
+          <Card key={announcement.id} className="hover:shadow-lg transition max-w-lg">
             <CardContent>
               <div className="flex items-start justify-between">
-                <div className="text-lg font-semibold">{c.title}</div>
+                <div className="text-lg font-semibold">{announcement.title}</div>
                 <div className="text-right">
                   <div className="text-sm text-muted-foreground">Enseignant</div>
-                  <div className="font-medium">{c.teacherName}</div>
+                  <div className="font-medium">{announcement.description}</div>
                 </div>
               </div>
               <div className="mt-4 flex justify-between">
@@ -62,8 +52,8 @@ export default function CoursesPage() {
                 <EmptyMedia variant={"icon"}>
                   <BookX className="w-14 h-14 text-green-400" />
                 </EmptyMedia>
-                <EmptyTitle>0 cours</EmptyTitle>
-                <EmptyDescription>Pas de cours pour le moment.</EmptyDescription>
+                <EmptyTitle>0 annonce</EmptyTitle>
+                <EmptyDescription>Pas d'annonce pour le moment.</EmptyDescription>
               </EmptyHeader>
             </Empty>
           </div>
